85dac7a2598100ee3532023f742fd56e7bee2b65
oscargop98 <59029173+oscargop98@users.noreply.github.com>
2022-10-27 17:11:35 -0500

    prueba

---
+++ casino.py
@@ -0,0 +1,20 @@
+// Encuentra el valor de π
+import random
+
+n = 1000000
+r = 1.0
+a, b = (0.0, 0.0)
+x_neg, x_pos = a - r, a + r
+y_neg, y_pos = b - r, b + r
+
+count = 0.0
+for i in range(0, n):
+
+    x = random.uniform(x_neg, x_pos)
+    y = random.uniform(y_neg, y_pos)
+    
+    if x*x + y*y <= 1.0:
+        count += 1
+
+result = (count / float(n)) * 4
+print ("result is ",result)
